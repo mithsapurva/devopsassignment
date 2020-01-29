bc8806812fbe165d6891f668b37e999894322435
Apurva Sawhney <apurva.sawhney@nagarro.com>
2020-01-29 13:19:31 +0530

    Added tests

---
+++ DevOpsAssignment.UnitTests/UnitTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevOpsAssignment.UnitTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.IsTrue(1 < 2);
+        }
+    }
+}
